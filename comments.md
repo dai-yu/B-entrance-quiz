11acfe477275c5a9faac2c89cc1bdf517d45c5ca
zhuyuqian <yuqian.zhu@thoughtworks.com>
2020-11-04 16:30:02 +0800

    [zhuyuqian]GTB comments

---
+++ comments.md
@@ -0,0 +1,38 @@
+### 完成度：
+
+
+__Details:__
+
+
+
+### 测试：
+
+
+__Details:__
+
+
+
+### 知识点：
+
+
+__Details:__
+
+- \- @CrossOrigin可以放在类上面
+- \- @RestController和@ResponseEntity重复
+- \- ResponseEntity用法错误
+
+### 工程实践：
+
+
+__Details:__
+
+- \- ResponseEntity应指定类型参数
+- \- for循环可以使用Collections.addAll()替换
+
+### 综合：
+* 完成度低，无法准确评估各个维度
+
+__Details:__
+
+
+
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/GroupController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/GroupController.java
@@ -14,6 +14,8 @@ public class GroupController {
     List<String> students= new LinkedList<>(Arrays.asList("成吉思汗","鲁班七号","太乙真人",
             "钟无艳","花木兰","雅典娜","芈月","白起","刘禅","庄周","马超","刘备","哪吒","大乔","蔡文姬"));
 
+    // TODO GTB-知识点: - @CrossOrigin可以放在类上面
+    // TODO GTB-知识点: - @RestController和@ResponseEntity重复
     @GetMapping("/students")
     @CrossOrigin(origins = {"http://localhost:1234"})
     public ResponseEntity<List<String>> getStudents(){
@@ -22,10 +24,12 @@ public class GroupController {
 
 
 
+    // TODO GTB-工程实践: - ResponseEntity应指定类型参数
     @GetMapping("/save")
     @CrossOrigin(origins = {"http://localhost:1234"})
     public ResponseEntity saveStudent(@RequestParam String student){
         students.add(student);
+        // TODO GTB-知识点: - ResponseEntity用法错误
         return ResponseEntity.created(null).build();
     }
 
@@ -33,6 +37,7 @@ public class GroupController {
     @CrossOrigin(origins = {"http://localhost:1234"})
     public ResponseEntity group(){
         List<String> newlist= new LinkedList<>();
+        // TODO GTB-工程实践: - for循环可以使用Collections.addAll()替换
         for(int i=0 ;i<students.size();i++){
             newlist.add(students.get(i));
         }
